b29e0f9cfd1d5231fd7ef18ac9569e9f2a22a275
NinhPc <ninhmphpl@gmail.com>
2022-09-12 21:27:19 +0700

    add

---
+++ Caulenh.js
@@ -0,0 +1,10 @@
+/** tùy chỉnh phần thập phân của float
+ * toFixed(), lấy phần thập của float, mặc định là 0
+ * vd a = 1.2324
+ * consoler.log(a.toFixed(2)) // 1.23
+ * toPrecision(), lấy phần thập phân tính từ giá trị đầu tiên
+ * vd a = 1.2324
+ * consoler.log(a.toPrecision(3)) // 1.23
+ * vd a = 0.034
+ * consoler.log(a.toPrecision(3)) // 0.0340
+ * */
